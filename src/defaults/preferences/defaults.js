f46680b3310d43da63484c287e22f46aed769668
Boris Capitanu <capitanu@illinois.edu>
2008-12-17 17:00:48 +0000

    - description and version changes

--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -1,1 +1,1 @@
-pref("extensions.zeasr.configURL", "http://leovip223.ncsa.uiuc.edu/zotero_seasr/flows.json");
+pref("extensions.zeasr.configURL", "http://repository.seasr.org/Zotero/config/leovip232.config");
