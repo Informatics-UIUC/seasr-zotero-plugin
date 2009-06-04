7d45724b7c4ce9c6072d5e9d3a7744bb8108b138
Boris Capitanu <capitanu@illinois.edu>
2009-06-04 21:47:20 +0000

    - changed default config file

--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -1,2 +1,1 @@
-pref("extensions.zeasr.configURL", "http://repository.seasr.org/Zotero/config/leovip232.config");
-pref("extensions.zeasr.configProviders", '[{"name":"SEASR", "url":"http://repository.seasr.org/Zotero/config/leovip232.config", "enabled":"true"}]');
+pref("extensions.zeasr.configProviders", '[{"name":"SEASR", "url":"http://repository.seasr.org/Zotero/config/seasr.config", "enabled":"true"}]');
