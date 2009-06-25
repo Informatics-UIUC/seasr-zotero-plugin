b1323fbc92c0402d3023ea47670f49107418770c
Boris Capitanu <capitanu@illinois.edu>
2009-06-25 07:43:51 +0000

    - default config file now in XML format

--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -1,1 +1,1 @@
-pref("extensions.zeasr.configProviders", '[{"name":"SEASR", "url":"http://repository.seasr.org/Zotero/config/seasr.config", "enabled":"true"}]');
+pref("extensions.zeasr.configProviders", '[{"name":"SEASR", "url":"http://repository.seasr.org/Zotero/config/seasr.xml", "enabled":"true"}]');
