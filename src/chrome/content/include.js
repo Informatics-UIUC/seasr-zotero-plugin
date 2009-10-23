b3d2e418f6363ce751e11a6ce229aae39307cc3d
Boris Capitanu <capitanu@illinois.edu>
2009-10-23 20:16:53 +0000

    - fix conflict issue between this extension and VUE (collision on the name 'jsLoader')

--- src/chrome/content/include.js
+++ src/chrome/content/include.js
@@ -1,10 +1,11 @@
-const jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-                    .getService(Components.interfaces.mozIJSSubScriptLoader);
-
-// get a reference to the Zotero instance
-jsLoader.loadSubScript("chrome://zotero/content/include.js");
-
-jsLoader.loadSubScript("chrome://zeasr/content/utils.js");
-jsLoader.loadSubScript("chrome://zeasr/content/zeasr.js");
-jsLoader.loadSubScript("chrome://zeasr/content/preferences/preferences.js");
-
+if (typeof(SEASR) == "undefined") {
+    var _jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+                        .getService(Components.interfaces.mozIJSSubScriptLoader);
+    
+    // get a reference to the Zotero instance
+    _jsLoader.loadSubScript("chrome://zotero/content/include.js");
+    
+    _jsLoader.loadSubScript("chrome://zeasr/content/utils.js");
+    _jsLoader.loadSubScript("chrome://zeasr/content/zeasr.js");
+    _jsLoader.loadSubScript("chrome://zeasr/content/preferences/preferences.js");
+}
